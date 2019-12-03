a7dadb009296ee02849aa9c39ee32fbd0981ed42
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2019-12-03 18:08:55 +0200

    last edit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+        function rotateElem() { 
+            document.querySelector('.main').style.transform 
+                       = 'rotateX(180deg)'; 
+        } 
+        function rotateEl() { 
+            document.querySelector('.main').style.transform 
+                       = 'rotateX(360deg)'; 
+        } 
